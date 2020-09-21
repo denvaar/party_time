3cb59e7386010567cc749fa9d39d8de52ff60e6a
Denver Smith <denverpsmith@gmail.com>
2020-09-20 18:56:03 -0600

    update elixir version

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule PartyTime.MixProject do
     [
       app: :party_time,
       version: "0.1.0",
-      elixir: "~> 1.7",
+      elixir: "~> 1.10.3",
       elixirc_paths: elixirc_paths(Mix.env()),
       compilers: [:phoenix, :gettext] ++ Mix.compilers(),
       start_permanent: Mix.env() == :prod,
